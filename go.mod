e1a64a8ca3a037a2fa1bbad487ce1ac64bc5e5de
Emil <emilbabazade29@gmail.com>
2025-12-14 03:15:02 +0200

    pause resume

--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.25
 require github.com/gen2brain/raylib-go/raylib v0.55.1
 
 require (
-	github.com/ebitengine/purego v0.7.1 // indirect
-	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
-	golang.org/x/sys v0.20.0 // indirect
+	github.com/ebitengine/purego v0.8.1 // indirect
+	github.com/gen2brain/raylib-go/raygui v0.0.0-20251212184511-13e94be231b3 // indirect
+	golang.org/x/exp v0.0.0-20241108190413-2d47ceb2692f // indirect
+	golang.org/x/sys v0.27.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 
+	rg "github.com/gen2brain/raylib-go/raygui"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func main() {
+	rl.SetConfigFlags(rl.FlagWindowResizable)
 	rl.InitWindow(800, 600, "Pong")
 	rl.SetTargetFPS(60)
 	rl.SetExitKey(rl.KeyEscape)
+	rg.SetStyle(rg.DEFAULT, rg.TEXT_SIZE, 32)
 
 	playerWidth := float32(25)
 	playerHeight := float32(100)
@@ -60,6 +63,10 @@ func main() {
 		ball.draw()
 		scoreText(scoreManager)
 
+		if gamePaused {
+			pauseMenu()
+		}
+
 		rl.EndDrawing()
 	}
 	rl.CloseWindow()
@@ -96,4 +103,40 @@ func resumeAll(items ...Pausable) {
 	for _, item := range items {
 		item.resume()
 	}
+}
+
+func pauseMenu() {
+	rg.Label(rl.Rectangle{X: 300, Y: 100, Width: 200, Height: 30}, "HELLO")
+	drawText("PONG", 100, 3)
+	rect := rl.Rectangle{
+		X:      100,
+		Y:      100,
+		Width:  200,
+		Height: 50,
+	}
+	rg.Button(rect, "RESUME")
+	//drawButton("RESUME", 50, 5, 0.05)
+	//drawButton("RESTART", 50, 6, 0.05)
+	//drawButton("QUIT", 50, 7, 0.05)
+}
+
+func drawButton(text string, fontSize int32, spacing int, padding float32) {
+	textWidth := float32(rl.MeasureText(text, fontSize) / 2)
+	rect := rl.Rectangle{
+		X:      float32(getHCenter(text, fontSize)) - float32(fontSize)*padding*2,
+		Y:      float32(rl.GetScreenHeight()/10*spacing) - float32(fontSize)*padding,
+		Width:  textWidth*2 + textWidth*2*padding,
+		Height: float32(fontSize) + float32(fontSize)*padding*2,
+	}
+	rl.DrawRectangleLinesEx(rect, 1, rl.White)
+	drawText(text, fontSize, spacing)
+}
+
+func drawText(text string, fontSize int32, spacing int) {
+	xCord := getHCenter(text, fontSize)
+	rl.DrawText(text, xCord, int32(rl.GetScreenHeight()/10*spacing), fontSize, rl.White)
+}
+
+func getHCenter(txt string, fontSize int32) int32 {
+	return int32(rl.GetScreenWidth()/2) - rl.MeasureText(txt, fontSize)/2
 }
